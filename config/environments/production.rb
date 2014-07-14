3228f04fb59b6d5adf9611f3559ab88a7d25d55d
embedtheta <sfgrahman@yahoo.com>
2014-07-14 14:54:33 +0600

    config update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ First::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 end
