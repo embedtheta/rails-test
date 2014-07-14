efc9910d37fcab238577de0a1f11106308729b55
embedtheta <sfgrahman@yahoo.com>
2014-07-14 13:09:29 +0600

    mail sending

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 First::Application.routes.draw do
+
+  get "emailer/index"
+  get "emailer/sendmail"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   resources :articles
+  resources :emailer
   root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
